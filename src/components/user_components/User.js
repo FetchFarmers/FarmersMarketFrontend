3445d54c698d2ba26efe6eb4597173ff8224af1d
Hkedge <hollyekedge@gmail.com>
2023-03-04 15:46:05 -0700

    added cart total

--- src/components/user_components/User.js
+++ src/components/user_components/User.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import UserProfile from './UserProfile';
