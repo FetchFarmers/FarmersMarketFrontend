86051a99cada448c2cdd944f04b32d264bf17ac0
Meanithhuon <huon.meanith@gmail.com>
2023-03-04 21:34:33 -0600

    comment out react lottie cause it crashed code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Lottie from 'react-lottie';
-import animationData from './Lotties/barn.json';
+// import Lottie from 'react-lottie';
+// import animationData from './Lotties/barn.json';
 
 function HomePage() {
     const defaultOptions = {
         loop: true,
         autoplay: true,
-        animationData: animationData,
+        // animationData: animationData,
         rendererSettings: {
           preserveAspectRatio: "xMidYMid slice"
         }
@@ -15,11 +15,11 @@ function HomePage() {
   return (
     <div className="mainBodyContainer">
       <h1 className='pageTitle' ></h1>
-      <Lottie 
+      {/* <Lottie 
 	    options={defaultOptions}
         height={400}
-        width={400}
-      />
+        width={400} */}
+      {/* /> */}
     </div>
   );
 }
