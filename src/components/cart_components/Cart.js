b83be366eea60166dc585fa088d846d81868abb4
Hkedge <hollyekedge@gmail.com>
2023-03-09 18:28:37 -0700

    Cart Products styling

--- src/components/cart_components/Cart.js
+++ src/components/cart_components/Cart.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowsRotate, faCartShopping } from '@fortawesome/free-solid-svg-icons'
-
+import { createPath } from 'react-router';
 
 import { 
   fetchRemoveOrderProduct,
@@ -13,6 +13,7 @@ import {
 } from '../../orders_api'; 
 
 
+
 function Cart({setCartItemTotal, cartItemTotal}) {
   const [userOrderProducts, setUserOrderProducts] = useState([])
   const [quantity, setQuantity] = useState(0);
@@ -20,38 +21,31 @@ function Cart({setCartItemTotal, cartItemTotal}) {
   const [orderId, setOrderId] = useState(0)
 
   const randomString =  () => {
-    return crypto.randomUUID()+"("+ new Date()+")"
+    return crypto.randomUUID()
   }
   
   async function loadUserOpenOrders() {
     try {  
       const sessionId = window.localStorage.getItem("fetchSessionId")
 
-      if (sessionId) {
-        const results = await fetchUserOpenOrders(sessionId);
-        setUserOrderProducts(() => results.products);
-        setOrderId(() => results.id)
-        console.log('loadOrderResults :>> ', results);
-        let numOfItems = 0
-        if (results.products) {
-          results.products.map((product) => numOfItems += product.quantity)
-          setCartItemTotal(numOfItems)
-          window.localStorage.setItem("cartTotal", numOfItems)
-        }
-
-      } else {
+      if (!sessionId) {
         const newSessionId = randomString();
         window.localStorage.setItem("fetchSessionId", newSessionId )
-        const results = await fetchUserOpenOrders(newSessionId);
-        setUserOrderProducts(() => results.products);
-        setOrderId(() => results.id)
-        let numOfItems = 0
-        if (results.products) {
-          results.products.map((product) => numOfItems += product.quantity)
-          setCartItemTotal(numOfItems)
-          window.localStorage.setItem("cartTotal", numOfItems)
-        }
+      }
+        
+      const results = await fetchUserOpenOrders(sessionId);
+      const resultProducts = results.products
+      const sortedProducts = resultProducts.sort((a, b) => (a.name > b.name) ? 1: -1);
+      console.log('sortedProducts :>> ', sortedProducts);
+      setUserOrderProducts(() => sortedProducts);
+      setOrderId(() => results.id)
+      console.log('loadOrderResults :>> ', results);
 
+      let numOfItems = 0
+      if (results.products) {
+        results.products.map((product) => numOfItems += product.quantity)
+        setCartItemTotal(numOfItems)
+        window.localStorage.setItem("cartTotal", numOfItems)
       }
 
     } catch (error) {
@@ -80,7 +74,9 @@ function Cart({setCartItemTotal, cartItemTotal}) {
 
   }
 
-  async function handleUpdateItem(orderProductId) {
+  async function handleUpdateItem(orderProductId, event) {
+    console.log('am I getting here?');
+
     try{
       const results = await fetchUpdateOrderProductQuantity (orderProductId, quantity)
       console.log('UpdateItemResults :>> ', results);
@@ -147,38 +143,41 @@ function Cart({setCartItemTotal, cartItemTotal}) {
     <div className='mainCartPage'>
       <h3 className='cartPageTitle' >Shopping Cart</h3>
       <div className='cartDetailsCtr'>
-        <div className="cartProductsCtr">
-          <h3 className="cartProductsCtrTitle">Products {cartItemTotal !== 0 && <>({cartItemTotal} {cartItemTotal > 1 ? <>items</> : <>item</>})</>}</h3>
-          {cartItemTotal === 0 && <h4 className='cartEmptyMessage' >Your cart is currently empty</h4>}
-          {userOrderProducts && <div>
-            {userOrderProducts.map((item) =>
-              (<div className='cartProductCtr'>
-                <div key={item.id}>
-                  <div className='cartProductCtrTop'>
-                    <p className='cartProdTitle'>{item.name} - ${item.price} each</p>
-                    <button className="editCartBtn" onClick={() =>handleRemoveItem(item.orderProductId)} >X</button>
-                  </div>  
-                  <div className='editCartBntCtr'>
-                    <h4>Quantity</h4>
-                    <input className='cartQtyDropdown' type='number' defaultValue={item.quantity} onChange={(event) => setQuantity(event.target.value)} min={1} max={item.inventory || 10}/>
-                    <button className="updateCartBtn" onClick={() => handleUpdateItem(item.orderProductId)}><FontAwesomeIcon  icon={faArrowsRotate}/></button>
-                    <h4 className='cartProdTotal'>Total: ${(item.price*item.quantity).toFixed(2)}</h4>
+        <div className='cartProductsOutsideCtr'>
+        <h3 className="cartProductsCtrTitle">Products {cartItemTotal !== 0 && <>({cartItemTotal} {cartItemTotal > 1 ? <>items</> : <>item</>})</>}</h3>
+          <div className="cartProductsCtr">
+            {cartItemTotal === 0 && <h4 className='cartEmptyMessage' >Your cart is currently empty</h4>}
+            {userOrderProducts && <div>
+              {userOrderProducts.map((item) =>
+                  (<div className='cartProductCtr'>
+                    <div key={item.id}>
+                      <div className='cartProductCtrTop'>
+                        <p className='cartProdTitle'>{item.name}<class className ='itemPrice'> - ${item.price} each</class></p>
+                        <button className="xFromCartBtn" onClick={() =>handleRemoveItem(item.orderProductId)} >X</button>
+                      </div>  
+                      <div className='cartQtyTotalCtr'>
+                        <h4 className='cartQtyTitle'>Quantity</h4>
+                        <form onClick={(event)=>handleUpdateItem(item.orderProductId)}>
+                        <input className='cartQtyDropdown' type='number' defaultValue={item.quantity} onChange={(event) => setQuantity(event.target.value)} min={1} max={item.inventory || 10}/>
+                        </form>
+                        <h4 className='cartProdTotal'>${(item.price*item.quantity).toFixed(2)}</h4>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
             ))}</div>}
-        </div>        
-        <div className="checkoutDetailsCtr">
-        <h3 className="cartProductsCtrTitle">Order Details</h3>
-              {cartItemTotal !== 0 &&<h3 className='sumTotal'>Products Total: ${orderSum.toFixed(2)}</h3>}
-              {cartItemTotal !== 0 &&<h3 className='shippingCharge'>Delivery Fee: $5.99</h3>}
-              {cartItemTotal !== 0 &&<h3 className='taxes'>Taxes: ${taxes.toFixed(2)}</h3>}
-                {cartItemTotal !== 0 &&<h3 className='orderTotal'>Order Total: ${(orderSum+taxes+5.99).toFixed(2)}</h3>}
-              <div className='manageCartBntCtr'>
-                { cartItemTotal !== 0 &&<button className="checkoutCartBtn" onClick={() => handleCheckout()} >Checkout</button>}
-                { cartItemTotal !== 0 &&<button className="cancelOrderBtn" onClick={() => handleCancelOrder()} >Cancel Order</button>}
-              </div>
-        </div>
+          </div>        
+        </div>  
+        {cartItemTotal !== 0 && <div className="checkoutDetailsCtr">
+          <h3 className="cartProductsCtrTitle">Order Details</h3>
+          <h3 className='sumTotal'>Products Total: ${orderSum.toFixed(2)}</h3>
+          <h3 className='shippingCharge'>Delivery Fee: $5.99</h3>
+          <h3 className='taxes'>Taxes: ${taxes.toFixed(2)}</h3>
+          <h3 className='orderTotal'>Order Total: ${(orderSum+taxes+5.99).toFixed(2)}</h3>
+          <div className='manageCartBtnCtr'>
+            <button className="checkoutCartBtn" onClick={() => handleCheckout()} >Checkout</button>
+            <button className="cancelOrderBtn" onClick={() => handleCancelOrder()} >Cancel Order</button>
+          </div>
+        </div>}
       </div>
     </div>
   );
