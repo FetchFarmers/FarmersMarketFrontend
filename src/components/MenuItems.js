e95bd5a8dfd111e9ce237da2b821bb0f1d549928
cvmug <catherine.mugnai@gmail.com>
2023-03-13 15:32:46 -0400

    added media queries for the header

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Dropdown from './Dropdown';
 import { Link } from 'react-router-dom';
 
